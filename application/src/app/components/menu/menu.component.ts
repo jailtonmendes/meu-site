75c286ac75a8877fd6aff1f5ae711e2cad494ba0
jailtonmendes <jailtonmendes.silva@gmail.com>
2023-04-11 00:32:02 -0300

    Projetando layout baseado no figma para o código

--- application/src/app/components/menu/menu.component.ts
+++ application/src/app/components/menu/menu.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  teste() {
+    console.log('testando...')
+  }
+
 }
--- application/src/app/shared/shared-primeNg/shared-primeNg.module.ts
+++ application/src/app/shared/shared-primeNg/shared-primeNg.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from "@angular/core";
 import { SplitterModule } from 'primeng/splitter';
 import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [],
   imports: [
     SplitterModule,
-    CardModule
+    CardModule,
+    ButtonModule
   ],
   exports: [
     SplitterModule,
-    CardModule
+    CardModule,
+    ButtonModule
   ]
 })
 export class SharedPrimeNgModule {}
