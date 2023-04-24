969b238b6a8be2d137d2ab375b02f38621a9fe3e
jailtonmendes <jailtonmendes.silva@gmail.com>
2023-04-24 01:57:37 -0300

    Finalizando Página inicial

--- application/src/app/components/body/body.component.ts
+++ application/src/app/components/body/body.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-body',
@@ -16,11 +18,35 @@ export class BodyComponent {
   card2!: boolean;
   card3!: boolean;
   card4!: boolean;
+  nomeBotao!: string;
 
   leiaMais!: boolean;
 
-  constructor(private router: Router) {
+  @HostBinding('style.width')
+  modalWidth = '50vw';
 
+  private resizeSubject = new Subject<void>();
+  private resize$: Observable<number> = this.resizeSubject.pipe(
+    debounceTime(100),
+    map(() => window.innerWidth),
+    distinctUntilChanged(),
+    shareReplay(1)
+  );
+
+  constructor(private router: Router) {
+    this.resize$.subscribe((width) => {
+      console.log(width)
+      this.updateModalWidth(width);
+    });
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.resizeSubject.next();
+  }
+
+  private updateModalWidth(width: number) {
+    this.modalWidth = width < 768 ? '100vw' : '50vw';
   }
 
   projetos() {
@@ -39,6 +65,7 @@ export class BodyComponent {
       this.card2 = false
       this.card3 = false
       this.card4 = false
+      this.nomeBotao = 'Demonstração'
     }
     if(id == 2) {
       descricao = 'Este projeto é um layout desenvolvido utilizando HTML e SASS, com o objetivo de criar uma interface atraente e funcional. A linguagem SASS permite uma escrita mais organizada e modular do CSS, resultando em um código mais limpo e fácil de manter.'
@@ -50,19 +77,33 @@ export class BodyComponent {
       this.card2 = true
       this.card3 = false
       this.card4 = false
+      this.nomeBotao = 'Demonstração'
     }
 
     if(id == 3) {
-      descricao = 'O projeto "Lista de Tarefas" foi desenvolvido utilizando as tecnologias Ionic, Angular e TypeScript. O objetivo principal do projeto é permitir que o usuário crie, gerencie e acompanhe suas tarefas diárias de forma simples e eficiente.'
+      descricao = "Este projeto foi desenvolvido durante o DartWeek, um evento realizado pela Academia do Flutter. Utilizamos a arquitetura MVC e o padrão Repository, juntamente com as tecnologias Provider e Bloc."
       this.visible = true;
       this.titulo = titulo;
       this.descricaoProjeto = descricao
-      this.link = 'http://jailtonmendes.dev.br/appTarefa'
+      this.link = 'https://github.com/jailtonmendes/dart-week-vakinha-burguer'
       this.card1 = false
       this.card2 = false
       this.card3 = true
       this.card4 = false
+      this.nomeBotao = 'Ver código'
     }
+
+    // if(id == 3) {
+    //   descricao = 'O projeto "Lista de Tarefas" foi desenvolvido utilizando as tecnologias Ionic, Angular e TypeScript. O objetivo principal do projeto é permitir que o usuário crie, gerencie e acompanhe suas tarefas diárias de forma simples e eficiente.'
+    //   this.visible = true;
+    //   this.titulo = titulo;
+    //   this.descricaoProjeto = descricao
+    //   this.link = 'http://jailtonmendes.dev.br/appTarefa'
+    //   this.card1 = false
+    //   this.card2 = false
+    //   this.card3 = true
+    //   this.card4 = false
+    // }
 
     if(id == 4) {
       descricao = 'Este é um aplicativo de turismo que desenvolvi como projeto de estudo, utilizando as tecnologias Ionic, Angular e TypeScript'
@@ -72,8 +113,9 @@ export class BodyComponent {
       this.link = 'https://jailtonmendes.dev.br/files-portfolio/projetos/app-turismo-ionic/'
       this.card1 = false
       this.card2 = false
-      this.card3 = true
-      this.card4 = false
+      this.card3 = false
+      this.card4 = true
+      this.nomeBotao = 'Demonstração'
     }
 
 
--- application/src/app/components/projetos/projetos.component.ts
+++ application/src/app/components/projetos/projetos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, shareReplay } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,16 +11,54 @@ import { Router } from '@angular/router';
 export class ProjetosComponent {
 
   visible!: boolean;
+  // innerWidth!: number;]
+  @HostBinding('style.width')
+  modalWidth = '50vw';
+
+  private resizeSubject = new Subject<void>();
+  private resize$: Observable<number> = this.resizeSubject.pipe(
+    debounceTime(100),
+    map(() => window.innerWidth),
+    distinctUntilChanged(),
+    shareReplay(1)
+  );
 
   constructor(private router: Router) {
+    // this.innerWidth = window.innerWidth;
+    // this.innerWidth$.
+    this.resize$.subscribe((width) => {
+      console.log(width)
+      this.updateModalWidth(width);
+    });
+  }
 
+  // @HostListener('window:resizer', ['$event'])
+  // onResizer(event: any) {
+  //   this.innerWidth = window.innerWidth
+  // }
+
+  // @HostBinding('style.width')
+  // get modalWidth() {
+  //   let width = window.innerWidth < 768 ? '100vw' : '50vw'
+  //   console.log(width)
+  //   // return this.innerWidth < 768 ? '100vw' : '50vw';
+  // }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.resizeSubject.next();
+  }
+
+  private updateModalWidth(width: number) {
+    this.modalWidth = width < 768 ? '100vw' : '50vw';
   }
 
 
 
-    showDialog() {
-        this.visible = true;
-    }
+  showDialog() {
+      this.visible = true;
+      // console.log('width: ', this.modalWidth)
+  }
 
   voltar() {
     this.router.navigate(['/']);
